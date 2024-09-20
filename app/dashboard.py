23ebd7d42f4407ae8f60dfac9d76f7e21a079119
Rodrigo Carvalho <rhrc09@gmail.com>
2024-09-20 16:09:16 -0300

    feat: display meals when the user logs in

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 
 from app.auth import login_required
+
+from app.db import get_db
 
 bp = Blueprint("dashboard", __name__)
 
@@ -8,4 +10,11 @@ bp = Blueprint("dashboard", __name__)
 @bp.route("/")
 @login_required
 def index():
-    return render_template("dashboard/home.html")
+    db = get_db()
+
+    meals = db.execute(
+        "SELECT pet.name, meal.datetime, meal.quantity, user.username FROM meal JOIN pet ON pet.id = meal.pet_id JOIN user ON user.id = meal.user_id WHERE meal.pet_id IN (SELECT pet_id FROM user_pet WHERE user_pet.user_id = ?)",
+        (g.user["id"],),
+    )
+
+    return render_template("dashboard/home.html", meals=meals)
